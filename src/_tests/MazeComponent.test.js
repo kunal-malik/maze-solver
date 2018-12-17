d060f93f5a68aaf3dcf6cce64efd6d7e8489e525
VARIANT\kunal.malik <kunal.malik@freestyleiot.com>
2018-12-17 14:45:28 +1100

    Formatting test case

--- src/_tests/MazeComponent.test.js
+++ src/_tests/MazeComponent.test.js
@@ -3,7 +3,7 @@ import MazeComponent from '../components/MazeComponent'
 import { shallow, mount } from 'enzyme'
 
 describe('RosterComponent component ', () => {
-    const validMaze = `###########
+  const validMaze = `###########
     S #   #   #
     # # # # # #
     #   #   # #
@@ -15,7 +15,7 @@ describe('RosterComponent component ', () => {
     #   #     F
     ###########`;
 
-    const solvedMaze = `###########
+  const solvedMazeStr = `###########
     S^#^^^#^^^#
     #^#^#^#^#^#
     #^^^#^^^#^#
@@ -27,7 +27,7 @@ describe('RosterComponent component ', () => {
     #   #^^^^^F
     ###########`
 
-    const invalidMaze = `###########
+  const invalidMaze = `###########
     S #   #   #
     # # # # # #
     #   #   # #
@@ -40,11 +40,11 @@ describe('RosterComponent component ', () => {
     ##########`;
 
   it('renders without crashing', () => {
-    shallow(<MazeComponent/>)
+    shallow(<MazeComponent />)
   });
 
   it('should render textarea for user to input maze', () => {
-    const wrapper = mount(<MazeComponent/>)
+    const wrapper = mount(<MazeComponent />)
     const container = wrapper.find('[id="maze-container"]')
     expect(container.exists()).toBe(true)
     const inputMazeTextArea = container.find('[id="input-maze"]')
@@ -56,7 +56,7 @@ describe('RosterComponent component ', () => {
   it('should call handleClick function on click of button', () => {
     spyOn(MazeComponent.prototype, 'handleClick').and.callThrough();
     spyOn(MazeComponent.prototype, 'solveMaze').and.callThrough();
-    const wrapper = mount(<MazeComponent/>)
+    const wrapper = mount(<MazeComponent />)
     const buttons = wrapper.findWhere(elment => elment.type() === 'button');
     const button = buttons.find('[data-test="button"]')
     button.simulate('click')
@@ -66,7 +66,7 @@ describe('RosterComponent component ', () => {
 
   it('should call resetAll function on click of button when maze has been solved', () => {
     spyOn(MazeComponent.prototype, 'resetAll').and.callThrough();
-    const wrapper = mount(<MazeComponent/>)
+    const wrapper = mount(<MazeComponent />)
     wrapper.setState({ solvedMaze: [[]] });
     const buttons = wrapper.findWhere(elment => elment.type() === 'button');
     const button = buttons.find('[data-test="button"]')
@@ -75,18 +75,18 @@ describe('RosterComponent component ', () => {
   })
 
   it('should display solved maze on click of button when user has entered a valid maze', () => {
-    const wrapper = mount(<MazeComponent/>)
+    const wrapper = mount(<MazeComponent />)
     wrapper.setState({ inputMaze: validMaze });
     const buttons = wrapper.findWhere(elment => elment.type() === 'button');
     const button = buttons.find('[data-test="button"]')
     button.simulate('click')
     const solvedMaze = wrapper.find('[id="solved-maze-container"]')
     expect(solvedMaze.exists()).toBe(true)
-    expect(solvedMaze.text() == solvedMaze);
+    expect(solvedMaze.text() == solvedMazeStr);
   })
 
   it('should display error message in case maze rows are not equal', () => {
-    const wrapper = mount(<MazeComponent/>)
+    const wrapper = mount(<MazeComponent />)
     wrapper.setState({ inputMaze: invalidMaze });
     const buttons = wrapper.findWhere(elment => elment.type() === 'button');
     const button = buttons.find('[data-test="button"]')
